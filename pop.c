be4ff6a222730cc176c6be7a1690df2b0941ee11
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-17 23:53:08 +0000

    removes the top element of the stack

---
+++ pop.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * pop - removes the top element of the stack
+ * @stack: linked lists for monty stack
+ * @num_line: number of line opcode occurs on
+ */
+void pop(stack_t **stack, unsigned int num_line)
+{
+	stack_t *actual = *stack;
+	printf("%s", stack);
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", num_line);
+		exit(EXIT_FAILURE);
+	}
+	*stack = actual->next;
+	if (actual->next != NULL)
+		actual->next->prev = NULL;
+	free(actual);
+}
