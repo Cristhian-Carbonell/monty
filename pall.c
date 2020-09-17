8e7fa9990aec41061d53c079f477e37754808e1a
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-17 16:53:15 +0000

    print all function

---
+++ pall.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * pall - print all function
+ * @stack: pointer to linked list stack
+ * @num_line: number of line opcode occurs on
+ */
+void pall(stack_t **stack, unsigned int num_line)
+{
+	stack_t *actual = *stack;
+	(void)num_line;
+
+	if (stack == NULL || *stack == NULL)
+		return;
+	while (actual != NULL)
+	{
+		printf("%d\n", actual->n);
+		actual = actual->next;
+	}
+}
