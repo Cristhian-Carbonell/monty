6da6c29d811c137feac44e1ea7d1b7667a6d5f0e
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-17 16:50:13 +0000

    main function of the program

---
+++ monty.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * main - main function of monty program
+ * @argc: number of arguments
+ * @argv: pointer to array of strings of arguments
+ *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+ */
+int main(int argc, char **argv)
+{
+	stack_t *stack = NULL;
+
+	if (argc != 2)
+	{
+		printf("Usage: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	read_file(argv[1], stack);
+	free_stack(&stack);
+	return (EXIT_SUCCESS);
+}
