ee9d272ac85b0d7f5597a2c51f8c6ec528b3bb93
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-18 03:56:48 +0000

    doesn't do anything

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - doesn't do anything
+ * @stack: linked lists for monty stack
+ * @num_line: number of line opcode occurs on
+ */
+void nop(stack_t **stack, unsigned int num_line)
+{
+	(void)stack;
+	(void)num_line;
+}
