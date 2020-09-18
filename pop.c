e430285cd03964164d316cf8885c8bd7388fbebf
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-18 00:21:21 +0000

    remove line code, for add line code

--- pop.c
+++ pop.c
@@ -7,15 +7,13 @@
  */
 void pop(stack_t **stack, unsigned int num_line)
 {
-	stack_t *actual = *stack;
+	stack_t *actual = (*stack)->next;
 
-	if ((actual == NULL) || (stack == NULL))
+	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", num_line);
 		exit(EXIT_FAILURE);
 	}
-	*stack = actual->next;
-	if (actual->next != NULL)
-		actual->next->prev = NULL;
-	free(actual);
+	free(*stack);
+	*stack = actual;
 }
